e8bc22299559359c7277415c9210994ae114b023
Pranjal Kumar <pranjalk13490@gmail.com>
2025-12-22 14:32:08 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+
+console.log(3
+    +
+    3)// code readability should be high 
+
+console.log("Pranjal")
+let name = "Pranjal" // string
+let age =18 // numeric
+let isLoggedIn=false //boolean
+let state;//undefined
+// Data types
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+// boolean=>true/false
+//null => standalone value
+// undefined=>
+//symbol => unique ness ko define karta hai ki ak component dusre se kese alag hai
+
+//object
+
+console.log(typeof "name") // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); //Object
+
