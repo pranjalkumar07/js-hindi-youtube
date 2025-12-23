b5b640423a71c616dba764148d0b4b8547087e7a
Pranjal Kumar <pranjalk13490@gmail.com>
2025-12-23 13:55:29 +0000

    operations discussed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);//number
+//console.log(valueInNumber);//Nan
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Pranjal"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn); //True
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber);  //string
+
+/***********************operations************ */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Pranjal"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(2 + "1"); //21
+// console.log("1" +2 +2); //122
+// console.log(1 +2 +"1"); //31
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // ye accha nahi h likhna khne ka mtlb hai ki coding ke ander readability jyada jaruri hai kyon hii likhna confusing code
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter); //101
+gameCounter++
+console.log(gameCounter); //102
+
+
+
+
+
+
+
+
+
+
