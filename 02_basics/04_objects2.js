96a18fd1d5e5d85a6b7eba689c5c291e40832a59
Pranjal Kumar <pranjalk13490@gmail.com>
2026-01-26 18:20:11 +0000

    objects and json

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,100 @@
+// const tinderUser = new Object()
+// console.log(tinderUser);//{}
+
+const tinderUser = {}
+// console.log(tinderUser);//{}
+
+tinderUser.id = "abc123"
+tinderUser.name = "wakka"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);//{ id: 'abc123', name: 'wakka', isLoggedIn: false }
+
+
+//Nested objects(object ke andar object)
+const regularUser = {
+    email : "Pranjal@gmail.com",
+    fullname : {
+        username : {
+            firstname : "Pranjal",
+            lastname : "Kumar"
+
+        }
+    }
+}
+console.log(regularUser.fullname.username);//{ firstname: 'Pranjal', lastname: 'Kumar' } mtlb apan kese bhi access kar sakte jo karna chahe . lagakar kar sakte hai
+console.log(regularUser.fullname.username.firstname); //Pranjal
+
+//OBJECTS KO MERGE KARNA
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1 , obj2}
+// console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } } this is not considered as good method as you can see ye obj ke andar obj ke hi form me de rha hai
+
+//object.assign( method)
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+//Best and modern :spread operator
+
+const obj3 = {...obj1,...obj2}
+console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+
+//ARRAY OF OBJECTS
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+// console.log(users[1].email)//h@gmail.com
+
+console.log(tinderUser) 
+// { id: 'abc123', name: 'wakka', isLoggedIn: false }
+console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)); //[ 'abc123', 'wakka', false ]
+console.log(Object.entries(tinderUser));////[ [ 'id', 'abc123' ], [ 'name', 'wakka' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));//true
+//hasown property basically true flase deta hai ki ye key is object me exist karti hai ya nahi
+
+//*******DESTRUCTURING */
+
+const course = {
+    coursename : "js in hindi",
+    price : 999,
+    courseInstructor : "Hitesh"
+}
+//NORMAL ACCESS
+//course.courseInstructor
+
+//Destructuring
+const {courseInstructor} = course
+// console.log(courseInstructor); //Hitesh
+
+//if you want to change courseInstructor
+
+const{courseInstructor : Instructor} = course
+console.log(Instructor);//IT also give Hitesh
+
+//JSON define karne ka tarika
+{
+    "name" : "Pranjal",
+    "courseName":"js in hindi",
+    "price" : "free"
+
+}
+//IN JSON key always in quotes,FUNCTIONS not allowed,used in data transfer
+
+
+
