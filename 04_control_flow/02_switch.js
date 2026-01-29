a892fbd5ecf3fae55a35a1cc99a3175ab8fc218e
Pranjal Kumar <pranjalk13490@gmail.com>
2026-01-29 18:18:48 +0000

    control flow(switch)

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,53 @@
+// basic syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+// //     default:
+// //         break;
+// // }
+
+// // const month = 3
+// //  switch (month) {
+//     case 1:
+//         // console.log("jan");
+//         break;
+//     case 2:
+//         // console.log("feb");
+//         break;
+//     case 3:
+//         // console.log("march");
+//         break;
+//     case 4:
+//         // console.log("april")
+//         break;
+ 
+//     default:
+//         // console.log("default");
+        
+//         break;
+//  }
+
+
+
+  const month = "march"
+ switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april")
+        break;
+ 
+    default:
+        console.log("default case match");
+        
+        break;
+ }
