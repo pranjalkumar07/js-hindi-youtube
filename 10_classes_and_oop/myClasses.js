26783d58d541bc5e70351627f7f04853fe681081
Pranjal Kumar <pranjalk13490@gmail.com>
2026-07-10 17:43:12 +0000

    about class

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,41 @@
+//ES6
+
+// class User{
+//     constructor(username,email,password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUserName(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai","pranjal@134","123")
+// console.log(chai.encryptPassword());//123abc
+// console.log(chai.changeUserName());//CHAI
+
+function user (username,email,password){
+    this.username=username;
+    this.email = email;
+    this.password = password;
+}
+
+user.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+user.prototype.changeUserName = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new user("tea","tea@1344","123");
+console.log(tea.encryptPassword());//123abc
+console.log(tea.changeUserName());//TEA
+
+
+
+
